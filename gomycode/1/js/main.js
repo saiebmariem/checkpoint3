4c34c6bc9a2bfe364e511dfcec8a99735a9ee51b
saiebmariem <52163812+saiebmariem@users.noreply.github.com>
2019-07-03 11:36:36 +0100

    Add files via upload

---
+++ gomycode/1/js/main.js
@@ -0,0 +1,16 @@
+function bold(){
+    document.getElementById("mytext").style.fontWeight="bold";
+    
+    }
+function itallic(){
+    document.getElementById("mytext").style.fontStyle="italic";
+}
+function underlined1(){
+    document.getElementById("mytext").style.textDecoration="underline";
+}
+function size1(){
+    document.getElementById("mytext").style.fontSize=document.getElementById("three").value;
+}
+function text1(){
+    document.getElementById("mytext").style.fontFamily=document.getElementById("four").value;
+}
